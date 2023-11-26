9f8dc41e1b50ea1a289f906e3466958397bb0847
izuchukwu obi <izuchukwuobi997@gmail.com>
2023-11-26 05:28:21 +0100

    highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreListSkeleton from "./GenreListSkeleton";
 
 
 interface Props {
-  onSelectGenre: (genre: Genre) => void
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null
 }
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
 	const { data, isLoading, error } = useGenres();
 
 	if (error) return null;
@@ -28,6 +29,7 @@ const GenreList = ({onSelectGenre}: Props) => {
 								src={getCroppedImageUrl(genre.image_background)}
 							/>
 							<Button
+                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
 								onClick={() => onSelectGenre(genre)}
 								fontSize="lg"
 								variant="link"
